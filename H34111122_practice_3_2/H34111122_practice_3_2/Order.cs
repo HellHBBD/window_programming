442cb2ef271ec701593016199969415da6774629
Hell HBBD <paulyu602@gmail.com>
2024-10-05 15:22:21 +0800

    add practice 3-2

---
+++ H34111122_practice_3_2/H34111122_practice_3_2/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H34111122_practice_3_2
+{
+    internal class Order
+    {
+        public int index, amount;
+        public string option;
+
+        public Order(int number, int amount, string option)
+        {
+            index = number;
+            this.amount = amount;
+            this.option = option;
+        }
+    }
+}
